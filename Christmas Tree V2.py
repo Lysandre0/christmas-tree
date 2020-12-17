6079fee6f95efe70fe2941f061abd12dd6cf83ec
Lysandre0 <73218452+Lysandre0@users.noreply.github.com>
2020-12-17 16:38:50 +0100

    Update Christmas Tree V2.py

--- Christmas Tree V2.py
+++ Christmas Tree V2.py
@@ -1,3 +1,6 @@
+"""
+Tree Function (Cette fonction décrit la forme des branches du sapin de Noël)
+"""
 def print_tree(n1, n2, n3):
     for size in range(1, n1, 2):
         print((size * "*").center(23))
@@ -7,6 +10,9 @@ def print_tree(n1, n2, n3):
         print((size * "*").center(23))
 print_tree(8, 16, 24)
 
+"""
+Trunk Function (Cette fonction décrit la forme du tronc du sapin de Noël)
+"""
 def print_trunk(n):
     for size in range(n):
         print((5 * "*").center(23))
