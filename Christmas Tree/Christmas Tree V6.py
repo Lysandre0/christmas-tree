06e2f181dd6a361afb67daa7c3f85ac07d0aad5b
Lysandre0 <73218452+Lysandre0@users.noreply.github.com>
2020-12-18 10:44:41 +0100

    Update Christmas Tree V6.py

--- Christmas Tree/Christmas Tree V6.py
+++ Christmas Tree/Christmas Tree V6.py
@@ -7,9 +7,9 @@
 toplevel = int(input("Entrez la taille du premier étage : "))
 middlelevel = int(input("Entrez la taille du second étage : "))
 bottomlevel = int(input("Entrez la taille du troisième étage : "))
-topgarlands = int((toplevel)//2.6)
-middlegarlands = int((middlelevel)//2.6)
-bottomgarlands = int((bottomlevel - 5)//4)
+topgarlands = int(((toplevel//2.5)-1)*1.14)
+middlegarlands = int(((middlelevel//2.5)-1)*1.14)
+bottomgarlands = int((bottomlevel - 5)//4 - 1)
 
 """
 Star Function (Cette fonction décrit la forme de l'étoile du sapin de Noël)
@@ -27,26 +27,26 @@ def print_star():
 Tree Function (Cette fonction décrit la taille des branches du sapin de Noël)
 """
 def print_tree(n1, n2, n3, n4):
-    for tree in range(1, n1, 2):
-        print((tree * "*").center(bottomlevel))
-    for tree in range(1, n2, 2):
-        print(((tree * "0")+(topgarlands * " ")+(3 * "*")+(topgarlands * " ")+(tree * "0")).center(bottomlevel))
-    for tree in range(7, n3, 4):
-        print((tree * "*").center(bottomlevel))
-    for tree in range(1, n2, 2):
-        print(((tree * "0")+(middlegarlands * " ")+(5 * "*")+(middlegarlands * " ")+(tree * "0")).center(bottomlevel))
-    for tree in range(11, n4, 6):
-        print((tree * "*").center(bottomlevel))
+    for size in range(1, n1, 2):
+        print((size * "*").center(bottomlevel))
+    for size in range(1, n2, 2):
+        print(((size * "0")+(topgarlands * " ")+(3 * "*")+(topgarlands * " ")+(size * "0")).center(bottomlevel))
+    for size in range(7, n3, 4):
+        print((size * "*").center(bottomlevel))
+    for size in range(1, n2, 2):
+        print(((size * "0")+(middlegarlands * " ")+(5 * "*")+(middlegarlands * " ")+(size * "0")).center(bottomlevel))
+    for size in range(11, n4, 6):
+        print((size * "*").center(bottomlevel))
 
 """
 Trunk Function (Cette fonction décrit la taille du tronc du sapin de Noël)
 """
 def print_trunk(n):
-    for trunk in range(5, n, 20):
+    for size in range(5, n, 20):
         print((((bottomgarlands * "| ")+(5 * "*")+(bottomgarlands * " |")).center(bottomlevel)))
-    for trunk in range(5, n, 20):
+    for size in range(5, n, 15):
         print(((bottomgarlands * "0 ")+(5 * "*")+(bottomgarlands * " 0")).center(bottomlevel))
-    for trunk in range(5, n, 20):
+    for size in range(5, n, 20):
         print((5* "*").center(bottomlevel))
 
 print_star()      
